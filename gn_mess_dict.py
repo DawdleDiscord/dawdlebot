e638e5611b9f5387595d40bd338abaaba4a62188
Alexander_Mercaldi <alexander.mercaldi@email.ucr.edu>
2019-10-11 13:02:34 -0700

    Adding current ugly version of dawdlebot

---
+++ gn_mess_dict.py
@@ -0,0 +1,10 @@
+gn_mess = {
+	
+	'nini_mess': [
+	'Goodnight, my sweet sweet love. I hope you have wonderful dreams about your lord and savior Kirby.',
+	'Do Dawdlers dream of electric crat? Rest up and find out.',
+	'sleep well my doggie'
+
+
+	]
+}
