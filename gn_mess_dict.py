45279e1043df475b1a4508f8461b772e1f2e84c9
amer <amercaldi14@gmail.com>
2019-10-13 21:36:44 -0700

    Added more messages

--- gn_mess_dict.py
+++ gn_mess_dict.py
@@ -1,9 +1,32 @@
 gn_mess = {
 	
 	'nini_mess': [
-	'Goodnight, my sweet sweet love. I hope you have wonderful dreams about your lord and savior Kirby.',
-	'Do Dawdlers dream of electric crat? Rest up and find out.',
-	'sleep well my doggie'
+	"Goodnight, my sweet sweet love. I hope you have wonderful dreams about your lord and savior Kirby.",
+	"Sweet dreams - we\'ll be in shortly to collect your sleep breath in a jar. Kidding. But your friends here at Dawdle will miss you!",
+	"May you dream of fairies and magic lands beyond your bedroom. May your favorite fictional characters chill and snack on gummies with you in these dreams. I love you.",
+	"May your pillow be soft, your blankets be warm, and your mind be filled with thoughts of how much we love you!",
+	"*sleep well my doggie, tonight you're gonna have one of those nights where you're like \"damn it can't get better than this\" but tomorrow you're gonna be like \"DAMN IT CAN GET BETTER THAN THAT\"*",
+	"I hope your day has been good, and you did your best. If you don't think you did your best, well, I'm here to cheer you on, let's get some sleep and make tomorrow yours!!",
+	"Sleep well, go dawdle in dreamland. You are loved and deserve the rest, you beautiful human.",
+	"The dream faeries of dawdle are here to ensure that your rest and your dreams are as perfect and lovely as you are.",
+	"It's not like I hope you have sweet dreams tonight or anything..I only want you to be well rested so that I can compliment roast you more tomorrow",
+	"May the shadows take your nightmares \n and render them into narwhals \n through a mystical process \n known only to them \n Goodnight Friend \nSleep Well \n Knowing that \n You arent alone \n You are loved by many\n Drift of to dreamland now. Sleep",
+	"Goodnight my sweet and lovely friend. I hope you have the sweetest dreams and that your days are filled with sunshine and happiness because you deserve the best.",
+	"Sweet dreams! **(Disclaimer: If saint is in your dream, keep it PG, otherwise she is not responsible for dismemberment. Thank you.)**",
+	"May your slumber be as bliss as the calmest oceans as your victories are like the roaring waves. May your pillow be as soft as the lightest cloud and your bed as comforting as a loving embrace. May your dreams be as creative as the artist who’s brush never touches the painting but blesses the mind. May your heart be as beautiful as it was today, for it is something in which I will never truly understand but will forever cherish. Goodnight.",
+	"i hope u are able to get amazing rest tonight because you deserve it the most. tomorrow will be an awesome day.",
+	"hi friendo c: hope u had an amazing day, if not then that's okay! sleep is the reset button, tomorrow is brand new so u can make it better. hope u sleep tight and remember not to keep ur toes out or saint will lick them. nini friendo <3",
+	"Nini friend, and sleep well knowing you have friends who wanna hear from you tomorrow and all days to come",
+	"Sweet dreams boo. We'll see you soon. No matter what the coming day brings, Dawdle's got you.",
+	"Whatever happens tomorrow, face it the best you can, but don't push yourself too hard. We care about your well being, so remember. Dawdle has your back!",
+	"sleep well, it is foretold that you will awaken feeling bright and fresh in the morning! have wonderful dreams",
+	"goodnight, sleep tight, and don’t let the bed bugs bite! if they do bite them back! sleep well, i hope you have sweet dreams",
+	"Goodnight you cutie patootie. Make tomorrow as amazing as you are, we all believe in you ",
+	"Good night and sweet dreams!~ You've had a long day, but you got through it! We're so proud of you, and you deserve this good rest!"
+
+
+
+
 
 
 	]
