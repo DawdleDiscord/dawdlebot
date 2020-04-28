9d08e2425a97e2af4d60518aa06aa58f12e56a0f
amer <amercaldi14@gmail.com>
2020-04-27 23:45:09 -0700

    Added lots of cogs

---
+++ db_clean.py
@@ -0,0 +1,96 @@
+import discord
+import json,typing
+from db_checks import is_mod
+from discord.ext import commands
+
+class clean(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+		with open('cleanbotlist.json', 'r') as json_file:
+			self.bot_dict = json.load(json_file)
+		del json_file
+		with open('cleanchannellist.json', 'r') as json_file:
+			self.channellist = json.load(json_file)
+		del json_file
+		self.botprefixes = self.bot_dict.values()
+
+	@commands.Cog.listener()
+	async def on_message(self, message):
+		if message.guild and message.channel.id in self.channellist:
+			is_command = False
+			for pref in self.botprefixes:
+				if message.content.startswith(pref):
+					is_command = True
+					break
+			if message.author.bot or is_command:
+				await message.delete()
+
+	@commands.group()
+	@is_mod()
+	async def clean(self, ctx):
+		if ctx.invoked_subcommand is None:
+			await ctx.send('Invalid clean command')
+
+	@clean.command()
+	async def botadd(self, ctx, botname : str, prefix : str):
+		self.bot_dict[botname] = prefix
+		with open('cleanbotlist.json', 'w') as json_file:
+			json.dump(self.bot_dict, json_file)
+		del json_file
+		await ctx.send('Bot added')
+
+	@clean.command()
+	async def botremove(self, ctx, botname : str):
+		del self.bot_dict[botname]
+
+		with open('cleanbotlist.json', 'w') as json_file:
+			json.dump(self.bot_dict, json_file)
+		del json_file
+		await ctx.send('Bot removed')
+
+	@botremove.error
+	async def botremove_error(self, ctx, error):
+		if isinstance(error, KeyError):
+			await ctx.send('I could not find that bot')
+
+	@clean.command()
+	async def botlist(self, ctx):
+		stringlist = []
+		for bot in self.bot_dict.keys():
+			botstring = f"❥ {bot}"
+			botstring = botstring.ljust(24)
+			botstring = botstring+self.bot_dict[bot]
+			stringlist.append(botstring)
+
+		botlist = '\n'.join(stringlist)
+		listEmbed = discord.Embed(title = '', description=botlist, color=0xffb6c1)
+		await ctx.send(embed=listEmbed)
+
+	@clean.command()
+	async def channeladd(self, ctx, channel : discord.TextChannel):
+		self.channellist.append(channel.id)
+		with open('cleanchannellist.json', 'w') as json_file:
+			json.dump(self.channellist, json_file)
+		del json_file
+		await ctx.send(f'Added {channel.mention}')
+
+	@clean.command()
+	async def channelremove(self, ctx, channel : discord.TextChannel):
+		self.channellist.remove(channel.id)
+		with open('cleanchannellist.json', 'w') as json_file:
+			json.dump(self.channellist, json_file)
+		del json_file
+		await ctx.send(f'Removed {channel.mention}')
+
+	@clean.command()
+	async def channellist(self, ctx):
+		channellist_str = []
+		for ch_id in self.channellist:
+			channel = ctx.guild.get_channel(ch_id)
+			channel_str = f"❥ {channel.mention}"
+			channellist_str.append(channel_str)
+		channellist_all = '\n'.join(channellist_str)
+		channelEmbed = discord.Embed(title = '', description = channellist_all, color=0xffb6c1)
+		await ctx.send(embed=channelEmbed)
+
+    
---
+++ db_fuzzies.py
@@ -0,0 +1,56 @@
+import discord
+import json,typing
+from db_checks import is_member
+from discord.ext import commands
+
+
+class fuzzies(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+		with open('fuzzies.json', 'r') as json_f_r:
+			fuzz_dict = json.load(json_f_r)
+		self.fchannel_id = fuzz_dict['channel']
+
+
+	@commands.command()
+	async def fuzzie(self, ctx, *, msg : str):
+
+		for guild in self.bot.guilds:
+			if guild.name == 'dawdle':
+				dawdle = guild
+				break
+		dbchannel = dawdle.get_channel(623016717429374986)
+		fuzEmbed = discord.Embed(title = 'Fuzzie', description = msg,color=0xffb6c1)
+		fuzEmbed.set_footer(text=ctx.author.id)
+		fuzAprv = await dbchannel.send(embed=fuzEmbed)
+		await fuzAprv.add_reaction('<:pinkcheck:609771973341610033>')
+		await fuzAprv.add_reaction('<:pinkx:609771973102534687>')
+		await ctx.send('Your fuzzie has been sent to staff for approval.')
+
+	@commands.Cog.listener()
+	async def on_raw_reaction_add(self, payload):
+		for guild in self.bot.guilds:
+			if guild.name == 'dawdle':
+				dawdle = guild
+				break
+		if payload.guild_id == 475584392740339712 and payload.channel_id == 623016717429374986:
+			dbchannel = dawdle.get_channel(623016717429374986)
+			fmess = await dbchannel.fetch_message(payload.message_id)
+			reactemoj = payload.emoji
+			if fmess.embeds and fmess.embeds[0].title == 'Fuzzie':
+				for react in fmess.reactions:
+					if react.emoji == reactemoj and react.count == 2:
+						if reactemoj.id == 609771973341610033:
+							fchannel = dawdle.get_channel(self.fchannel_id)
+							fuzzie = fmess.embeds[0].description
+							fEmbed = discord.Embed(title = '', description=fuzzie,color=0xffb6c1)
+							finalfMess = await fchannel.send(embed=fEmbed)
+							await finalfMess.add_reaction('❤')
+							await fmess.delete(delay=2.0)
+
+						elif reactemoj.id == 609771973102534687:
+							fuzzMem = await dawdle.fetch_member(fmess.embeds[0].footer.text)
+							await fuzzMem.send('Your fuzzie has been denied because it did not follow the rules. Please review them before trying again.')
+
+
+
